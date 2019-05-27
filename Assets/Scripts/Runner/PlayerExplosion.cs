e6efcccd7df1b1a9ae4f34090a64bf6467326761
OhHiMark <Zervanseryi@gmail.com>
2019-05-27 15:20:19 +0300

    explosion fix

--- Assets/Scripts/Runner/PlayerExplosion.cs
+++ Assets/Scripts/Runner/PlayerExplosion.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class PlayerExplosion: MonoBehaviour
 {
+    private Renderer playerRenderer;
     public ParticleSystem playerExplosion;
 
     private void Start()
     {
         GameController.Instance.GameOverEvent.AddListener(OnPlayerDeath);
+        playerRenderer = GetComponent<Renderer>();
     }
 
     private void OnPlayerDeath()
     {
+        //GameObject obstacleParticles = Instantiate(playerExplosion.gameObject, transform.position, Quaternion.identity) as GameObject;
         GameObject obstacleParticles = Instantiate(playerExplosion.gameObject, transform.position, Quaternion.identity) as GameObject;
+        Color color = new Color(playerRenderer.material.color.r, playerRenderer.material.color.g, playerRenderer.material.color.b, 1f);
+        var main = obstacleParticles.GetComponent<ParticleSystem>().main;
+        main.startColor = color;
+        obstacleParticles.GetComponent<ParticleSystem>().Play();
         Destroy(gameObject);
     }
 }
